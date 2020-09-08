706219183740c8c5e4c52b89507c9ed4aba776ba
user12212 <52378510+user12212@users.noreply.github.com>
2020-09-08 14:56:42 +0300

    add models Category and Product, create superuser

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=15, blank=False, null=False)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=40, blank=False, null=False)
+    price = models.FloatField(blank=False, null=False)
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
