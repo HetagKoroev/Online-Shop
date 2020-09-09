6713df207e5edc6be8bd5bc8c85bc7499a21af4d
user12212 <52378510+user12212@users.noreply.github.com>
2020-09-09 11:28:08 +0300

    add empty class view

--- home/views.py
+++ home/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>Домашняя страница</h1>')
+
+
+class Home(View):
+    pass
