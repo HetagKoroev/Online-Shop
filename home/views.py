a573a991aaa735dbcfc6bbf257456d765048a84d
user12212 <52378510+user12212@users.noreply.github.com>
2020-09-10 17:12:46 +0300

    add first template, some changes in home/views.py

--- home/views.py
+++ home/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from .models import Service, Category, ConcreteProduct
 
 
 def home(request):
-    return HttpResponse('<h1>Домашняя страница</h1>')
+    all_concrete_products = ConcreteProduct.objects.all()
+    context = {'concrete_products': all_concrete_products}
+    template = 'index.html'
+    return render(request, template_name=template, context=context)
 
 
 class Home(View):
