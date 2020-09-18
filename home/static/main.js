63d9b26bb030fce0760e2b5950d5d2c1ca1135fb
user12212 <52378510+user12212@users.noreply.github.com>
2020-09-18 10:16:17 +0300

    fix bugs in js code

--- home/static/main.js
+++ home/static/main.js
@@ -3,9 +3,14 @@ document.querySelectorAll('.service-item').forEach(service => {
 	service.addEventListener('click', (e) => {
 		document.querySelector('.categories').classList.add('show')
 		document.querySelectorAll('.category-item').forEach(category => {
-			if(category.dataset.id === '1' && service.dataset.id === '1') {
+			if(category.dataset.id === service.dataset.id) {
 				category.classList.add('show')
-			}
+			} else {
+                     category.classList.remove('show')
+                     document.querySelectorAll('.product-item').forEach(product => {
+                        product.classList.remove('show')
+                     })
+			       }
 		})
 	})
 })
@@ -15,9 +20,11 @@ document.querySelectorAll('.category-item').forEach(category => {
 	category.addEventListener('click', (e) => {
 		document.querySelector('.products').classList.add('show')
 		document.querySelectorAll('.product-item').forEach(product => {
-			if(product.dataset.id === '1' && category.dataset.id === '1') {
+			if(product.dataset.id === category.dataset.id) {
 				product.classList.add('show')
-			}
+			} else {
+			        product.classList.remove('show')
+			       }
 		})
 	})
 })
