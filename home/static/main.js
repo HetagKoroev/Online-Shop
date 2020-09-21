5ad104e2e95e57b930a0e5877bd280d96958257b
user12212 <52378510+user12212@users.noreply.github.com>
2020-09-21 10:36:55 +0300

    Fix styles, separate .js and .css from index.html

---
+++ home/static/main.js
@@ -0,0 +1,30 @@
+console.log('000')
+document.querySelectorAll('.service-item').forEach(service => {
+	service.addEventListener('click', (e) => {
+		document.querySelector('.categories').classList.add('show')
+		document.querySelectorAll('.category-item').forEach(category => {
+			if(category.dataset.myid === service.dataset.myid) {
+				category.classList.add('show')
+			} else {
+                     category.classList.remove('show')
+                     document.querySelectorAll('.product-item').forEach(product => {
+                        product.classList.remove('show')
+                     })
+			       }
+		})
+	})
+})
+
+
+document.querySelectorAll('.category-item').forEach(category => {
+	category.addEventListener('click', (e) => {
+		document.querySelector('.products').classList.add('show')
+		document.querySelectorAll('.product-item').forEach(product => {
+			if(product.dataset.myid === category.dataset.myid) {
+				product.classList.add('show')
+			} else {
+			        product.classList.remove('show')
+			       }
+		})
+	})
+})
