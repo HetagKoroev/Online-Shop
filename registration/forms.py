aecf6ea63bf4e9b9e0fae4fffeb39b90eb74c30c
user12212 <52378510+user12212@users.noreply.github.com>
2020-11-16 11:33:53 +0300

    Rename registration Form labels

--- registration/forms.py
+++ registration/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class RegistrationFrom(forms.Form):
-    username = forms.CharField(required=True, widget=forms.TextInput())
-    password = forms.CharField(required=True, widget=forms.PasswordInput())
+    username = forms.CharField(required=True, widget=forms.TextInput(), label='Имя')
+    password = forms.CharField(required=True, widget=forms.PasswordInput(), label='Пароль')
     email = forms.EmailField(required=True, widget=forms.EmailInput())
-    phone_number = forms.CharField(required=False, widget=forms.TextInput())
+    phone_number = forms.CharField(required=False, widget=forms.TextInput(), label='Телефон')
