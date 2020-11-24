da14e4a8b8d42d86873253e9e1e7b64c2100b585
user12212 <52378510+user12212@users.noreply.github.com>
2020-11-24 09:41:08 +0300

    Fix typing

--- home/views.py
+++ home/views.py
@@ -1,15 +1,17 @@
-from typing import Union
+from typing import Union, Type
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from .models import Platform, Category, ConcreteService
 from services.logic import get_all_objects_from_model
+from django.contrib.auth.decorators import login_required
 
 
+# @login_required
 def home(request: HttpRequest) -> Union[HttpResponse, None]:
     if request.method == 'GET':
-        platforms = get_all_objects_from_model(Platform)
-        categories = get_all_objects_from_model(Category)
-        concrete_services = get_all_objects_from_model(ConcreteService)
+        platforms = get_all_objects_from_model(Type[Platform])
+        categories = get_all_objects_from_model(Type[Category])
+        concrete_services = get_all_objects_from_model(Type[ConcreteService])
 
         context = {'platforms': platforms,
                    'categories': categories,
