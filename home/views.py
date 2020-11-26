2e886e4126653a8443792849741183b90f51e70a
user12212 <52378510+user12212@users.noreply.github.com>
2020-11-26 11:54:00 +0300

    Minor refactor 'home' view, turn-off APP_DIRS in settings

--- home/views.py
+++ home/views.py
@@ -2,18 +2,18 @@ from typing import Union, Type
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from .models import Platform, Category, ConcreteService
-from services.logic import get_all_objects_from_model
 from django.contrib.auth.decorators import login_required
 
 
 # @login_required
 def home(request: HttpRequest) -> Union[HttpResponse, None]:
     if request.method == 'GET':
-        platforms = get_all_objects_from_model(Type[Platform])
-        categories = get_all_objects_from_model(Type[Category])
-        concrete_services = get_all_objects_from_model(Type[ConcreteService])
+        platforms = Platform.objects.all()
+        categories = Category.objects.all()
+        concrete_services = ConcreteService.objects.all()
 
-        context = {'platforms': platforms,
+        context = {
+                   'platforms': platforms,
                    'categories': categories,
                    'concrete_services': concrete_services
                    }
--- services/logic.py
+++ services/logic.py
@@ -1,7 +1,5 @@
 from django.db.models.query import QuerySet
 from django.db.models import Model
+from typing import Type
 
 
-def get_all_objects_from_model(model: Model) -> QuerySet:
-    all_objects = model.objects.all()
-    return all_objects
