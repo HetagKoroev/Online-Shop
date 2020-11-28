dc16a9d6c16d3990f39bb56cad81c8e591a1d3c5
Hetag <het.koroev@gmail.com>
2020-11-28 20:21:29 +0300

    Fix 'static' directory path in settings

--- my_site/settings.py
+++ my_site/settings.py
@@ -113,6 +113,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'home/static/'
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
+print(STATIC_ROOT)
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
